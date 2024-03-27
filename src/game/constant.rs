2a4175866c64638cf1a1c5bcf08ab5b16d1ef92e
AardhynLavender <aardhyn@gmail.com>
2024-03-27 20:25:25 +1300

    fix: map and window size mismatch

--- src/game/constant.rs
+++ src/game/constant.rs
@@ -1,6 +1,10 @@
 use crate::engine::geometry::{Rec2, Vec2};
 use crate::engine::utility::alignment::Aligner;
 
+// Tile //
+
+pub const TILE_SIZE: Vec2<u32> = Vec2::new(16, 16);
+
 // Menu //
 
 pub const TITLE_Y: f32 = 80.0;
@@ -16,7 +20,8 @@ pub const TYPEFACE_SIZE: u16 = 5;
 // Window //
 
 pub const WINDOW_TITLE: &str = "Metroidvania";
-pub const LOGICAL_SIZE: Vec2<u32> = Vec2::new(640, 362);
+pub const LOGICAL_SIZE_TILES: Vec2<u32> = Vec2::new(40, 22);
+pub const LOGICAL_SIZE: Vec2<u32> = Vec2::new(LOGICAL_SIZE_TILES.x * TILE_SIZE.x, LOGICAL_SIZE_TILES.y * TILE_SIZE.y);
 pub const WINDOW_SCALE: f32 = 2.0;
 pub const WINDOW_SIZE: Vec2<u32> = Vec2::new(
   (LOGICAL_SIZE.x as f32 * WINDOW_SCALE) as u32,
