f9b9e686f8349db58487dd70d1dfa9527e0c3e40
AardhynLavender <aardhyn@gmail.com>
2024-04-04 18:51:04 +1300

    fix: fps in microseconds

--- src/engine/internal.rs
+++ src/engine/internal.rs
@@ -1,10 +1,11 @@
 use hecs::Entity;
 use sdl2::keyboard::Keycode;
 
-use crate::engine::component::text::Text;
-use crate::engine::render::color::color;
-use crate::engine::render::system::sys_render;
+use crate::engine::component::text::{Text, TextLayer};
+use crate::engine::rendering::color::color;
+use crate::engine::rendering::system::sys_render;
 use crate::engine::system::{Schedule, SysArgs, SystemManager};
+use crate::engine::utility::alias::DeltaMS;
 use crate::engine::world::World;
 use crate::game::physics::position::Position;
 
@@ -15,6 +16,8 @@ use crate::game::physics::position::Position;
 struct FpsText;
 
 static mut FPS_TEXT: Option<Entity> = None;
+static mut MIN_FPS: DeltaMS = DeltaMS::MAX;
+static mut MAX_FPS: DeltaMS = DeltaMS::MIN;
 
 /// Add internal systems to the system manager
 pub fn add_internal_systems(systems: &mut SystemManager) {
@@ -25,9 +28,9 @@ pub fn add_internal_systems(systems: &mut SystemManager) {
 
 /// Add internal entities to the world
 pub fn add_internal_entities(world: &mut World) {
-  let fps_text = Text::new(color::TEXT3);
+  let fps_text = Text::new(color::TEXT);
   unsafe {
-    FPS_TEXT = Some(world.add((FpsText, fps_text, Position::new(4.0, 4.0))));
+    FPS_TEXT = Some(world.add((FpsText, fps_text, Position::new(4.0, 4.0), TextLayer {})));
   }
 }
 
@@ -38,11 +41,21 @@ fn sys_fullscreen_toggle(SysArgs { render, event, .. }: &mut SysArgs) {
   }
 }
 
+pub const SECOND_MS: DeltaMS = 10_000.0;
+
 fn sys_update_fps_text(SysArgs { delta, world, .. }: &mut SysArgs) {
   unsafe {
     if let Some(entity) = FPS_TEXT {
       let fps_text = world.query_entity::<&mut Text>(entity).expect("Failed to get fps text");
-      fps_text.set_content(format!("FPS {:.4}", delta));
+      if *delta < MIN_FPS {
+        MIN_FPS = *delta;
+      }
+      if *delta > MAX_FPS {
+        MAX_FPS = *delta;
+      }
+      let fps_string = format!("FPS {:0>6.2} SLOW {:0>3.0} FAST {:0>3.0}", *delta * SECOND_MS, MIN_FPS * SECOND_MS, MAX_FPS * SECOND_MS);
+      // println!("{}", fps_string);
+      fps_text.set_content(fps_string);
     }
   }
 }
