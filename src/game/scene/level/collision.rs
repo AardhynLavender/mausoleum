243d17ff013c4f64e81ffb097464f17cae5e2b77
AardhynLavender <aardhyn@gmail.com>
2024-04-19 18:51:29 +1200

    feat: tile destruction

--- src/game/scene/level/collision.rs
+++ src/game/scene/level/collision.rs
@@ -1,12 +1,25 @@
+* Collision system for entities with room tiles
+* /
+
 use std::collections::HashMap;
 
-use crate::engine::geometry::collision::{Collision, CollisionBox, rec2_collision};
+use hecs::Entity;
+
+use crate::engine::geometry::collision::{Collision, CollisionBox, CollisionMask, rec2_collision};
+use crate::engine::geometry::shape::Vec2;
+use crate::engine::rendering::color::{OPAQUE, RGBA};
 use crate::engine::system::SysArgs;
 use crate::engine::tile::tile::{Tile, TileCollider};
+use crate::engine::tile::tilemap::{TileHandle, TileQuery};
+use crate::engine::utility::direction::{Direction, DIRECTIONS, HALF_ROTATION, QUARTER_ROTATION, Rotation};
 use crate::engine::world::World;
-use crate::game::physics::collision::{Collider, Fragile};
+use crate::game::physics::collision::{Collider, Fragile, make_collision_box};
 use crate::game::physics::position::Position;
 use crate::game::physics::velocity::Velocity;
+use crate::game::player::combat::{Bullet, Rocket};
+use crate::game::scene::level::meta::{Soft, Strong};
+use crate::game::scene::level::room::use_room;
+use crate::game::utility::controls::{Behaviour, Control, is_control};
 
 /// Maximum number of collision resolution attempts before panicking
 pub const MAX_COLLISION_PHASES: u32 = 10;
@@ -16,8 +29,8 @@ pub const MAX_COLLISION_PHASES: u32 = 10;
 pub struct RoomCollision;
 
 /// Resolve tile collisions for entities clickable with rooms tiles
-pub fn sys_tile_collision(SysArgs { world, .. }: &mut SysArgs) {
-  let entities = world
+pub fn sys_tile_collision(SysArgs { world, state, .. }: &mut SysArgs) {
+  let colliders = world
     .query::<(&Position, &Collider, &RoomCollision)>().with::<&RoomCollision>()
     .into_iter()
     .map(|(entity, (position, collider, ..))| {
@@ -25,14 +38,42 @@ pub fn sys_tile_collision(SysArgs { world, .. }: &mut SysArgs) {
     })
     .collect::<HashMap<_, _>>();
 
-  for (entity, (position, collider)) in &entities {
+  let room = use_room(state);
+
+  for (entity, (position, collider)) in &colliders {
     let mut collision_box = make_collision_box(position, collider);
     let mut phase = 0;
     'resolving: loop {
       phase += 1;
       let collision = get_tile_collisions(world, &collision_box).next();
-      if let Some(collision) = collision {
+      if let Some((tile, collision, position)) = collision {
         if phase > MAX_COLLISION_PHASES { panic!("Infinite collision resolution loop detected, what do?"); }
+
+        let strong = world.has_component::<Strong>(tile).expect("Failed to retrieve the entity");
+        let soft = world.has_component::<Soft>(tile).expect("Failed to retrieve the entity");
+        let bullet = world.has_component::<Bullet>(*entity).expect("Failed to retrieve the entity");
+        let rocket = world.has_component::<Rocket>(*entity).expect("Failed to retrieve the entity");
+        if strong && rocket || (soft && (rocket || bullet)) {
+          let result = room.query_tile(TileQuery::Position(position.0));
+          if let Ok(handle) = TileHandle::try_from(result) {
+            let mut check = Direction::Up;
+            for _ in 0..DIRECTIONS / 2 {
+              let check_result = room.query_tile(TileQuery::Coordinate(handle.coordinate + check.to_coordinate()));
+              if let Ok(handle) = TileHandle::try_from(check_result) {
+                if !world.has_component::<TileCollider>(handle.entity).expect("Failed to check tile tile collider") {
+                  let collision_box = CollisionBox::new(Vec2::default(), handle.concept.data.src.size);
+                  world.add_components(handle.entity, (TileCollider::new(collision_box, CollisionMask::default()), )).expect("Failed to add tile collider");
+                }
+                let mut tile_collider = world.get_component_mut::<TileCollider>(handle.entity).expect("Failed to retrieve tile collider");
+                tile_collider.mask.set_side(check.rotate(Rotation::Left, HALF_ROTATION), true).expect("failed to set side");
+              }
+              check = check.rotate(Rotation::Left, QUARTER_ROTATION);
+            }
+            room.remove_tile(world, handle);
+          } else {
+            panic!("No concept associated with destroyed tile");
+          }
+        }
 
         let fragile = world.has_component::<Fragile>(*entity).expect("Failed to retrieve the entity");
         if fragile {
@@ -42,7 +83,6 @@ pub fn sys_tile_collision(SysArgs { world, .. }: &mut SysArgs) {
 
         let mut position = world.get_component_mut::<Position>(*entity).expect("Failed to retrieve entity");
         let mut velocity = world.get_component_mut::<Velocity>(*entity).expect("Failed to retrieve entity");
-
         let resolution = collision.get_resolution();
         position.0 = position.0 - resolution;
         if resolution.y > 0.0 && velocity.0.y > 0.0 {
@@ -71,17 +111,30 @@ pub fn sys_tile_collision(SysArgs { world, .. }: &mut SysArgs) {
 }
 
 /// Get all tile collisions for a given collision box
-fn get_tile_collisions<'a>(world: &'a mut World, collider_box: &'a CollisionBox) -> impl Iterator<Item=Collision> + 'a {
+fn get_tile_collisions<'a>(world: &'a mut World, collider_box: &'a CollisionBox) -> impl Iterator<Item=(Entity, Collision, Position)> + 'a {
   world.query::<(&Position, &TileCollider, &Tile)>()
     .into_iter()
-    .filter_map(|(_, (tile_position, tile_collider, ..))| {
+    .filter_map(|(entity, (tile_position, tile_collider, ..))| {
       let tile_rect = &CollisionBox::new(tile_position.0 + tile_collider.collision_box.origin, tile_collider.collision_box.size);
-      rec2_collision(collider_box, tile_rect, tile_collider.mask)
+      let collision = rec2_collision(collider_box, tile_rect, tile_collider.mask);
+      if let Some(collision) = collision { Some((entity, collision, *tile_position)) } else { None }
     })
 }
 
-/// Create a worldspace collision box from a position and collider
-pub fn make_collision_box(position: &Position, collider: &Collider) -> CollisionBox {
-  CollisionBox::new(position.0 + collider.0.origin, collider.0.size)
-}
+/// Render the tile colliders to the screen when debug mode is active
+pub fn sys_render_tile_colliders(SysArgs { world, camera, render, event, .. }: &mut SysArgs) {
+  if !is_control(Control::Debug, Behaviour::Held, event) {
+    return;
+  }
 
+  for (_, (position, collider)) in world.query::<(&Position, &TileCollider)>() {
+    let color = RGBA::new(255, 0, 0, OPAQUE);
+    let (width, height) = collider.collision_box.size.destructure();
+    let p = camera.translate(Vec2::from(position.0 + collider.collision_box.origin));
+
+    if collider.mask.top { render.draw_line(p, p + Vec2::new(width as i32, 0), color); }
+    if collider.mask.right { render.draw_line(p + Vec2::new(width as i32, 0), p + Vec2::new(width as i32, height as i32), color); }
+    if collider.mask.bottom { render.draw_line(p + Vec2::new(0, height as i32), p + Vec2::new(width as i32, height as i32), color); }
+    if collider.mask.left { render.draw_line(p, p + Vec2::new(0, height as i32), color); }
+  }
+}
