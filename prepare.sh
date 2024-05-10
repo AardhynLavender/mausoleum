b3dc863f2a776d17fb5468fd79eccbfae2130bcb
AardhynLavender <aardhyn@gmail.com>
2024-05-10 16:01:46 +1200

    chore: improve

--- prepare.sh
+++ prepare.sh
@@ -3,12 +3,14 @@
 # Remove save data and copy the assets folder to the target directory
 
 target=target/$1;
+save_file="user_save.json";
 
 if [ ! -d "$target" ]; then
   echo "Target directory not found";
   exit 1;
 fi;
 
-rm -f "$target/user_save.json";
+rm -f "$save_file" "$target/$save_file"
 cp -r asset "$target";
+
 exit 0;
